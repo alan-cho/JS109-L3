050baf04cd161c17c6baeeafa47973b6c2b70049
Alan Cho <alancho397@gmail.com>
2023-07-02 01:54:47 -0500

    Added player and computer's turn

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -29,4 +29,55 @@ function initializeBoard() {
   return boardValues;
 }
 
-initializeBoard();
+function getPlayersPiece() {
+  //Validates the user's input
+  while (true) {
+    prompt("Choose your piece: X or O");
+    playersPiece = readline.question().toUpperCase();
+    if (playersPiece === "X") {
+      computersPiece = "O"; //What does it mean it doesn't get read?
+      break;
+    } else if (playersPiece === "O") {
+      computersPiece = "X";
+      break;
+    }
+    console.log("Please enter a valid piece.");
+  }
+
+  return playersPiece, computersPiece;
+}
+
+function playersTurn(piece) {
+  while (true) {
+    prompt("Select the square you'd like to play: 1-9");
+    let square = readline.question.trim();
+    if (boardValues[square] === " ") {
+      makeMove(square, piece, boardValues);
+      break;
+    }
+    console.log("Please enter a valid square.");
+  }
+
+  return;
+}
+
+function makeMove(square, piece, boardValues) {
+  boardValues[square] = `${piece}`;
+  return;
+}
+
+function computersTurn(piece, boardValues) {
+  while (true) {
+    let square = Math.floor(Math.random() * boardValues.length);
+    if (boardValues[square] === " ") {
+      makeMove(square, piece, boardValues);
+      break;
+    }
+  }
+
+  return;
+}
+
+let isPlayersTurn;
+let playersPiece;
+let computersPiece;
